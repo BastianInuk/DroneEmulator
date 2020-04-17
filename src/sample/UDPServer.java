bbb9a334a1ddda147ea5877976986414cd574091
Bastian Inuk Christensen <bastian@inuk.blog>
2020-04-17 13:17:27 +0200

    renamed class to reflect filename change

--- src/sample/UDPServer.java
+++ src/sample/UDPServer.java
@@ -1,4 +1,15 @@
 package sample;
 
-public class UDBServer {
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
+public class UDPServer
+{
+    private DatagramPacket udpSocket;
+    private int port;
+
+    public
 }
